115e41a9683c57f61d4b8bd352b187385bdcf56d
imyusi_ <zayyan3002@gmail.com>
2025-01-06 08:26:28 +0700

    Fix Code
    fix login regist

--- lib/Pages/Home/home.dart
+++ lib/Pages/Home/home.dart
@@ -1,5 +1,5 @@
 import 'package:empedu/services/auth_service.dart'; // Disesuaikan dengan nama package Anda
-import 'package:empedu/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
